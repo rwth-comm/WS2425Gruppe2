8bd1476e8e6c99e0a6c8a6ec8205fbf0b0c7e508
MoniqueBeck <Monique.beckers@rwth-aachen.de>
2025-01-16 20:01:52 +0100

    Update 16.01.

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -96,38 +96,6 @@ data <- bind_cols(raw.short, scores$scores)
 # Daten exportieren ----
 write_rds(data, "data/data.rds")
 
-
-
-### Poweranalyse ###
-
-install.packages("pwr")
-library(pwr)
-
-# Variante 1: Stichprobengröße gesucht ----
-pwr::pwr.t.test(n = NULL, sig.level = 0.05 , d = 0.8, power = 0.8)
-
-# Variante 2: Signifikanzniveau gesucht ----
-pwr::pwr.t.test(n = 200, sig.level = NULL , d = 0.2, power = 0.8)
-
-# Variante 3: Effektstärke gesucht ----
-pwr::pwr.t.test(n = 110, sig.level = 0.05 , d = NULL, power = 0.8)
-
-# Variante 4: Power gesucht ----
-
-install.packages("dataforsocialscience")
-install.packages("lsr")
-library(dataforsocialscience)
-library(tidyverse)
-library(lsr)
-df <- robo_care
-df_male <- filter(df, df$gender == "male")
-df_female <- filter(df, df$gender == "female")
-
-t.test(df_male$privacy_concerns, df_female$privacy_concerns)
-cohensD(df_male$privacy_concerns, df_female$privacy_concerns)
-
-pwr::pwr.t.test(n = 110, sig.level = 0.05, d = NULL , power = 0.5)
-
 # Qualitätskontrolle ----
 
 raw.short.quality <- careless_indices(raw.short, likert_vector = c(7:39), duration_column = "Duration", speeder_analysis = "median/2")
